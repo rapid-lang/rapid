387242a9b3ca210397cf1851cc51bf54ce247d06
Nate Brennand <natebrennand@gmail.com>
2014-11-17 14:12:16 -0500

    alter parser tests to allow for failing tests

--- compiler/parser_tests.sh
+++ compiler/parser_tests.sh
@@ -7,12 +7,28 @@ all_successful="0"
 
 for file in $parser_tests
 do
-    if ./parser < "$file" 2> /dev/null | grep -q $success
+    # switch on if a file should fail or not
+    if echo "$file" |  grep -q "fail_"
     then
-        echo "success: $file worked"
+
+        # should fail
+        if ./parser < "$file" 2> /dev/null | grep -q $success
+        then
+            echo "FAIL:    $file"
+            all_successful="1"
+        else
+            echo "success: $file worked"
+        fi
     else
-        echo "FAIL:    test $file"
-        all_successful="1"
+
+        # should not fail
+        if ./parser < "$file" 2> /dev/null | grep -q $success
+        then
+            echo "success: $file worked"
+        else
+            echo "FAIL:    test $file"
+            all_successful="1"
+        fi
     fi
 done
 
