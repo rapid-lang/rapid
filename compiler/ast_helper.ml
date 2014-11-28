4aba9df4d6fd02a26c63d9e4673f0d029fb2eba6
Nate Brennand <natebrennand@gmail.com>
2014-11-28 13:39:08 -0800

    standardize ast printing to one style

--- compiler/ast_helper.ml
+++ compiler/ast_helper.ml
@@ -31,21 +31,23 @@ let bin_op_s = function
 
 (* Prettyprint expressions *)
 let rec expr_s = function
-    | IntLit(l) -> "Literal " ^ string_of_int l
-    | Id(s) -> "Id " ^ s
-    | Binop(e1, o, e2) -> sprintf "Binop (%s) %s (%s)"
+    | IntLit(l) -> sprintf "(Int Literal (%d))" l
+    | Id(s) -> sprintf "(Id %s)" s
+    | Binop(e1, o, e2) -> sprintf "(Binop (%s) %s (%s))"
         (expr_s e1)
         (bin_op_s o)
         (expr_s e2)
-    | Assign(v, e) -> sprintf "Assign %s (%s)" v
+    | Assign(v, e) -> sprintf "(Assign %s (%s))"
+        v
         (expr_s e)
-    | Call(f, es) -> sprintf "Call %s [%s]" f
+    | Call(f, es) -> sprintf "(Call %s [%s])"
+        f
         (concat ", " (List.map (fun e -> sprintf "(%s)" (expr_s e)) es))
     | BoolVal(b) -> string_of_bool b
     | StringLit(s) -> s
     | Noexpr -> "Noexpr"
 
-let bin_op = function
+let bin_op_s = function
     | Add -> "+"
     | Sub -> "-"
     | Mult -> "*"
@@ -62,7 +64,7 @@ let rec string_of_expr = function
     | Id(s) -> s
     | Binop(e1, o, e2) -> sprintf "%s %s %s"
         (string_of_expr e1)
-        (bin_op o)
+        (bin_op_s o)
         (string_of_expr e2)
     | Assign(v, e) -> sprintf "%s = %s" v
         (string_of_expr e)
@@ -80,10 +82,10 @@ let string_of_t = function
     | User_def -> "var" (*TODO: change user def to something else*)
 
 let output_s = function
-    | Printf(f, el) -> sprintf "Printf(%s, %s)"
+    | Printf(f, el) -> sprintf "(Printf(%s, %s))"
         f
         (String.concat ", " (List.map expr_s el))
-    | Println(el) -> sprintf "Println(%s)\n"
+    | Println(el) -> sprintf "(Println(%s))"
         (String.concat ", " (List.map expr_s el))
 
 let string_of_vdecl (t, nm, e) = sprintf "%s %s %s\n"
@@ -95,27 +97,32 @@ let string_of_vdecl (t, nm, e) = sprintf "%s %s %s\n"
 
 (* Prettyprint statements *)
 let rec stmt_s = function
-    | Block(ss) -> sprintf "Block [%s]"
-        (concat ",\n" (List.map (fun s -> sprintf "(%s)" (stmt_s s)) ss))
-    | Expr(e) -> sprintf "Expr (%s)"
+    | Block(ss) -> sprintf "(Block {\n%s\n})"
+        (concat "\n" (List.map (fun s -> sprintf "(%s)" (stmt_s s)) ss))
+    | Expr(e) -> sprintf "(Expr (%s))"
         (expr_s e)
-    | If(e, s1, s2) -> sprintf "If (%s) (%s) (%s)"
+    | If(e, s1, Ast.Block([])) -> sprintf "(If (%s) -> (%s))"
+        (expr_s e)
+        (stmt_s s1)
+    | If(e, s1, s2) -> sprintf "(If (%s)\n(%s) else (%s))"
         (expr_s e)
         (stmt_s s1)
         (stmt_s s2)
-    | For(e1, e2, e3, s) -> sprintf "For (%s) (%s) (%s) (%s)"
+    | For(e1, e2, e3, s) -> sprintf "(For ((%s); (%s); (%s))\n{(%s)})"
         (expr_s e1)
         (expr_s e2)
         (expr_s e3)
         (stmt_s s)
-    | While(e, s) -> sprintf "While (%s) (%s)"
+    | While(e, s) -> sprintf "(While (%s)\n{(%s))0"
         (expr_s e)
         (stmt_s s)
-    | Output(o) -> output_s o
-    | VarDecl(vd) -> string_of_vdecl vd
+    | Output(o) -> sprintf "(Output (%s))"
+        (output_s o)
+    | VarDecl(vd) -> sprintf "(VarDecl (%s))"
+        (string_of_vdecl vd)
 
 let fstmt_s = function
-    | Return(e) -> sprintf "Return (%s)"
+    | Return(e) -> sprintf "(Return (%s))"
         (expr_s e)
     | FStmt(s) -> stmt_s s
 
@@ -125,74 +132,6 @@ let func_decl_s f = sprintf "{\nfname = \"%s\"\nformals = [%s]\n\tbody = [%s]\n}
     (concat ",\n" (List.map fstmt_s f.body))
 
 let program_s (stmts, funcs) = sprintf "([%s],\n%s)"
-    (concat ", " (List.map stmt_s stmts))
+    (concat "\n" (List.map stmt_s stmts))
     (concat "\n" (List.map func_decl_s funcs))
-
-let bin_op = function
-    | Add -> "+"
-    | Sub -> "-"
-    | Mult -> "*"
-    | Div -> "/"
-    | Equal -> "=="
-    | Neq -> "!="
-    | Less -> "<"
-    | Leq -> "<="
-    | Greater -> ">"
-    | Geq -> ">="
-
-let rec string_of_expr = function
-    | IntLit(l) -> string_of_int l
-    | Id(s) -> s
-    | Binop(e1, o, e2) -> sprintf "%s %s %s"
-        (string_of_expr e1)
-        (bin_op o)
-        (string_of_expr e2)
-    | Assign(v, e) -> sprintf "%s = %s" v
-        (string_of_expr e)
-    | Call(f, el) -> sprintf "%s(%s)" f
-        (concat ", " (List.map string_of_expr el))
-    | BoolVal(b) -> string_of_bool b
-    | StringLit(s) -> s
-    | Noexpr -> ""
-
-let rec string_of_stmt = function
-    | Block(stmts) -> sprintf "{\n%s}\n"
-        (str_concat (List.map string_of_stmt stmts))
-    | Expr(expr) -> sprintf "%s\n"
-        (string_of_expr expr)
-    | If(e, s, Block([])) -> sprintf "if (%s)\n%s"
-        (string_of_expr e)
-        (string_of_stmt s)
-    | If(e, s1, s2) -> sprintf "if (%s)\n%s else\n%s"
-        (string_of_expr e)
-        (string_of_stmt s1)
-        (string_of_stmt s2)
-    | For(e1, e2, e3, s) -> sprintf "for (%s; %s; %s) %s"
-        (string_of_expr e1)
-        (string_of_expr e2)
-        (string_of_expr e3)
-        (string_of_stmt s)
-    | While(e, s) -> sprintf "while (%s) %s"
-        (string_of_expr e)
-        (string_of_stmt s)
-    | Output(o) -> output_s o
-    | VarDecl(vd) -> string_of_vdecl vd
-
-let string_of_fstmt = function
-    | Return(e) -> sprintf "Return (%s)"
-        (expr_s e)
-    | FStmt(s) -> string_of_stmt s
-
-let string_of_fdecl fdecl =
-    sprintf "func %s(%s)(%s)\n{\n%s}\n"
-        (fdecl.fname)
-        (concat ", " fdecl.formals)
-        (concat ", " (List.map string_of_t fdecl.return))
-        (*(str_concat (List.map string_of_vdecl fdecl.locals))*)
-        (str_concat (List.map string_of_fstmt fdecl.body))
-
-let string_of_program (stmts, funcs) =
-    sprintf "%s\n%s"
-        (str_concat (List.map string_of_stmt stmts))
-        (concat "\n" (List.map string_of_fdecl funcs))
 
--- compiler/test_parser.ml
+++ compiler/test_parser.ml
@@ -1,7 +1,7 @@
 
 let lexbuf = Lexing.from_channel stdin in
 let program = Parser.program Scanner.token lexbuf in
-let program_str = Ast_helper.string_of_program program in
+let program_str = Ast_helper.program_s program in
     print_endline program_str;;
 
 print_endline "TEST_SUCCESS";;
