a48c9b6142e418186fa4d042c527813d5e99174d
Nate Brennand <natebrennand@gmail.com>
2014-12-01 18:30:04 -0500

    better naming

--- compiler/sast_helper.ml
+++ compiler/sast_helper.ml
@@ -14,25 +14,27 @@ let expr_option_map func = function
 
 
 module StringMap = Map.Make(String)
+
 let empty_symbol_table = StringMap.empty
+
 let symbol_table_list = StringMap.empty :: []
 
 
 (* inserts a (symbol -> type) into the top level scope *)
 let add_sym t id = function
-    | top_scope :: lst ->
-        if StringMap.mem id top_scope
+    | current_scope :: scope_list ->
+        if StringMap.mem id current_scope
             then raise ExistingSymbolErr
-            else (StringMap.add id t top_scope) :: lst
+            else (StringMap.add id t current_scope) :: scope_list
     | _ -> raise MissingSymbolTablesErr
 
 
 (* retrieves a type in the top level scope that it is found *)
 let rec get_type id = function
-    | top_scope :: lst ->
-        if StringMap.mem id top_scope
-            then StringMap.find id top_scope
-            else get_type id lst
+    | current_scope :: scope_list ->
+        if StringMap.mem id current_scope
+            then StringMap.find id current_scope
+            else get_type id scope_list
     | _ -> raise(VariatbleNotDefinedErr(Format.sprintf "%s is not defined" id))
 
 
@@ -42,6 +44,6 @@ let new_scope sym_tbl = empty_symbol_table :: sym_tbl
 
 (* pops the last added scope removing all scoped variables. *)
 let pop_scope = function
-    | top_scope :: scope_list -> scope_list
+    | current_scope :: scope_list -> scope_list
     | [] -> raise MissingSymbolTablesErr
 
