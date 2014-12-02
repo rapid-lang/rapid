f1545c14728b9bd651498f851d8e15a5d569a186
Nate Brennand <natebrennand@gmail.com>
2014-12-01 19:28:31 -0500

    check first arg in printf to be string

--- compiler/semantic_check.ml
+++ compiler/semantic_check.ml
@@ -13,6 +13,7 @@ exception InvalidTypeReassignErr of string
 exception UnsupportedExpressionType
 exception UnsupportedSexpr
 exception UnsupportedDatatypeErr
+exception StringDatatypeRequiredErr
 
 
 (* Takes a symbol table and sexpr and rewrites variable references to be typed *)
@@ -26,6 +27,15 @@ let rec rewrite_sexpr st = function
     | xpr -> xpr
 
 
+let rewrite_string_sexpr st = function
+    | SId id -> (
+        match get_type st id with
+        | String -> SExprString(SStringVar id)
+        | _ -> raise StringDatatypeRequiredErr)
+    | SExprString xpr -> SExprString xpr
+    | _ -> raise UnsupportedSexpr
+
+
 (* checks that an assignment has the proper types *)
 let check_var_assign_use sym_tbl id xpr =
     let t = get_type sym_tbl id in
@@ -37,7 +47,7 @@ let check_var_assign_use sym_tbl id xpr =
 
 (* rewrites any sexprs in an SOutput statement *)
 let check_s_output sym_tbl = function
-    | SPrintf(s, xpr_l) -> SPrintf((rewrite_sexpr sym_tbl s), List.map (rewrite_sexpr sym_tbl) xpr_l)
+    | SPrintf(s, xpr_l) -> SPrintf((rewrite_string_sexpr sym_tbl s), List.map (rewrite_sexpr sym_tbl) xpr_l)
     | SPrintln(xpr_l) -> SPrintln(List.map (rewrite_sexpr sym_tbl) xpr_l)
 
 
